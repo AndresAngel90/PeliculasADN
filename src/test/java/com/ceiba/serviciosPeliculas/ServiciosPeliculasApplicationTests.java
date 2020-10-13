3f8ed14cb3ceca75391ac155309800cb5727f7a1
Jairo Andrés Angel Morales - Ceiba Software <andres.angel@ceiba.com.co>
2020-10-13 08:56:20 -0500

    Cambio config jenkins file y gradle.build

--- src/test/java/com/ceiba/serviciosPeliculas/ServiciosPeliculasApplicationTests.java
+++ src/test/java/com/ceiba/serviciosPeliculas/ServiciosPeliculasApplicationTests.java
@@ -1,12 +1,12 @@
 package com.ceiba.serviciosPeliculas;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ServiciosPeliculasApplicationTests {
 
-	@Test
+	
 	void contextLoads() {
 	}
 
