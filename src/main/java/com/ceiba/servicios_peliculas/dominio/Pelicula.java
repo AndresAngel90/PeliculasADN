913b0bde7ae3b149560820251153924528dad674
Jairo Andrés Angel Morales - Ceiba Software <andres.angel@ceiba.com.co>
2020-10-20 14:46:07 -0500

    se trata de reducir codeSmell

--- src/main/java/com/ceiba/servicios_peliculas/dominio/Pelicula.java
+++ src/main/java/com/ceiba/servicios_peliculas/dominio/Pelicula.java
@@ -1,14 +1,7 @@
 package com.ceiba.servicios_peliculas.dominio;
 
-import com.ceiba.servicios_peliculas.dominio.excepcion.PeliculaExcepcion;
 
 public class Pelicula {
-	
-	private static final  String ERRORID = "el id no puede ser un valor negativo";
-	private static final  String FECHAERROR = "Hay error en la fecha por favor verifique";
-	private static final  String NOMBREPELICULAERROR = "El nombre de la pelicula es vacio o es mayor a 100 caracteres";
-	private static final  String URLERROR = "La url de la imagen es vacio o es mayor a 1000 caracteres";
-	private static final  String SINOPSISERROR = "La sinopsis de la pelicula es vacio o es mayor a 255 caracteres";
 	
 	private long peliculaID;
 	
@@ -21,11 +14,7 @@ public class Pelicula {
 	private String sinopsis;
 
 	public Pelicula(long peliculaID, String tituloPelicula, String fechaEstreno, String urlImagen, String sinopsis) {
-		validarIdPelicula(peliculaID);
-		validaNombrePelicula(tituloPelicula);
-		validaFecha(fechaEstreno);
-		validaURLImg(urlImagen);
-		validaSinopsis(sinopsis);
+		
 		this.peliculaID = peliculaID;
 		this.tituloPelicula = tituloPelicula;
 		this.fechaEstreno = fechaEstreno;
@@ -33,35 +22,7 @@ public class Pelicula {
 		this.sinopsis = sinopsis;
 	}
 	
-	private void validarIdPelicula(long id) {
-		if(id<0) {
-			throw new PeliculaExcepcion(ERRORID);
-		}
-	}
 	
-	private void validaFecha(String fecha) {
-		if(("").equals(fecha)) {
-			throw new PeliculaExcepcion(FECHAERROR);
-		}
-	}
-	
-	private void validaNombrePelicula(String nombrePelicula) {
-		if(("").equals(nombrePelicula) || nombrePelicula.length()>100 ) {
-			throw new PeliculaExcepcion(NOMBREPELICULAERROR);
-		}
-	}
-	
-	private void validaURLImg(String url) {
-		if(("").equals(url) || url.length()>1000 ) {
-			throw new PeliculaExcepcion(URLERROR);
-		}
-	}
-	
-	private void validaSinopsis(String sinopsis) {
-		if(("").equals(sinopsis) || sinopsis.length()>255 ) {
-			throw new PeliculaExcepcion(SINOPSISERROR);
-		}
-	}
 
 	public long getPeliculaID() {
 		return peliculaID;
